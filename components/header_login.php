20734d8884c65983de49d67e7b9431363d375211
Rizwan Hamka <ahmadrizwanhamka@mail.ugm.ac.id>
2025-05-30 18:31:13 +0700

    add npm run dev

--- components/header_login.php
+++ components/header_login.php
@@ -6,9 +6,10 @@
         <a href="?page=home" <?php if ($_GET['page'] == 'home') echo 'style="font-weight:700"'; ?>>Home</a>
         <a href="?page=shop" <?php if (in_array($_GET['page'], ['shop', 'shop-clothing', 'shop-accesory'])
 ) echo 'style="font-weight:700"'; ?>>Shop</a>
-        <a href="?page=contact" <?php if ($_GET['page'] == 'contact') echo 'style="font-weight:700"'; ?>>Contact</a>
+        <a href="?page=profile" <?php if ($_GET['page'] == 'profile') echo 'style="font-weight:700"'; ?>>Profile</a>
     </nav>
-    <a href="?page=dashboard" class="login" <?php if ($_GET['page'] == 'login') echo 'style="font-weight:700"'; ?>>
-        <span class="dot"></span>Profile
+    <a href="?page=dashboard" class="login" <?php if (($_GET['page'] ?? '') == 'login') echo 'style="font-weight:700"'; ?>>
+        <span class="dot"></span>Hi, <?php echo explode(' ', $_SESSION['user_name'])[0]; ?>!
     </a>
+
 </header>
